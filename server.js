16ec6a28c47a5a074ab53316b1f78ebf81bd1b5e
Jesse Bernier <jesse_bernier@hotmail.com>
2018-01-19 16:59:40 -0500

    server.js changes for heroku push

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+//var PORT = 3000;
+var port = process.env.PORT || 3000;
+
 
 // Initialize Express
 var app = express();
@@ -114,6 +116,7 @@ app.post("/articles/:id", function(req, res) {
 });
 
 // Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+//app.listen(PORT, function() {
+app.listen(port, function() {
+  console.log("App running on port " + port + "!");
 });
